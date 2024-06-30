91d8fe3962b74026819702bc2a80341c44076f0f
Michael Sullivan <sullivanmichael344@gmail.com>
2024-06-29 23:28:46 -0400

    Add UI

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const Gameboard = (function (){
     const board = [];
     let rows = 3;
     let columns = 3;
+    const getRows = () => rows;
+    const getColumns = () => columns;
     const getBoard = () => board;
     const displayBoard = () => {
         
@@ -31,10 +33,34 @@ const Gameboard = (function (){
           }
           return true;
       }
+      const resetBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+              board[i][j] = "";
+            }
+          }
+      }
 
-    return {board, place_marker, getBoard, displayBoard, checkTie}
+    return {board, place_marker, getBoard, displayBoard, checkTie, getRows, getColumns, resetBoard}
 })();
 
+const displayController = (function () {
+    const displayBoard = (rows, cols, par) => {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+              const div = document.createElement("div");
+              div.dataset.row = i;
+              div.dataset.col = j;
+              div.classList.add("cell");
+              par.appendChild(div);
+
+            }
+          }
+    }
+   
+    
+    return {displayBoard}
+})();
 
 
 
@@ -45,31 +71,32 @@ function createPlayer(name, marker){
     return {name, marker, make_move}
 }
 
+function checkGameWon(marker){
+    //creates subarrays for each winning combo
+    const topRow = Gameboard.board[0];
+    const midRow = Gameboard.board[1];
+    const botRow = Gameboard.board[2];
+    const leftCol = [topRow[0], midRow[0], botRow[0]];
+    const midCol = [topRow[1], midRow[1], botRow[1]];
+    const rightCol = [topRow[2], midRow[2], botRow[2]];
+    const diag_one = [topRow[0], midRow[1], botRow[2]];
+    const diag_two = [topRow[2], midRow[1], botRow[0]];
+
+    //checks if player's marker occupies a winning subarray combo
+    const isWin = (currentValue) => currentValue === marker;
+   if (topRow.every(isWin) || midRow.every(isWin) || botRow.every(isWin)
+  || leftCol.every(isWin) || midCol.every(isWin)||rightCol.every(isWin)
+|| diag_one.every(isWin)||diag_two.every(isWin)){
+    return true;
+}
+    
+}
+
+
+
 
 
 function gameController(){
-    
-    function checkGameWon(marker){
-        //creates subarrays for each winning combo
-        const topRow = Gameboard.board[0];
-        const midRow = Gameboard.board[1];
-        const botRow = Gameboard.board[2];
-        const leftCol = [topRow[0], midRow[0], botRow[0]];
-        const midCol = [topRow[1], midRow[1], botRow[1]];
-        const rightCol = [topRow[2], midRow[2], botRow[2]];
-        const diag_one = [topRow[0], midRow[1], botRow[2]];
-        const diag_two = [topRow[2], midRow[1], botRow[0]];
-
-        //checks if player's marker satisfies a winning subarray combo
-        const isWin = (currentValue) => currentValue === marker;
-       if (topRow.every(isWin) || midRow.every(isWin) || botRow.every(isWin)
-      || leftCol.every(isWin) || midCol.every(isWin)||rightCol.every(isWin)
-    || diag_one.every(isWin)||diag_two.every(isWin)){
-        return true;
-    }
-        
-    }
-
 
     
     let player_one = createPlayer("player 1", "X");
@@ -81,53 +108,68 @@ function gameController(){
     const switchPlayer = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
-    const activePlayerMove = (row, col) =>{
-        
-        activePlayer.make_move(row, col);
-        Gameboard.displayBoard();
-      
-        
-    }
+
+    const boardContainer = document.querySelector(".board-container");
+    displayController.displayBoard(Gameboard.getRows(), Gameboard.getColumns(), boardContainer);
+    
+   const cells = document.querySelectorAll(".board-container div");
+    
+    const startButton = document.querySelector(".start-button");
 
     const playGame = () => {
+        activePlayer = players[0];
+        Gameboard.resetBoard();
        
-        let keepGoing = true
+        cells.forEach(function(cell){
+            cell.textContent = "";
+            
+       }) 
+            
+      let gameOver = false;
+  
+            
+      boardContainer.addEventListener("click", (e) => {
+        if (gameOver) return;
 
-        while (keepGoing){
 
-        let row = prompt(`${activePlayer.name}, Choose row (0-2)`);
-        let col = prompt(`${activePlayer.name}, Choose column (0-2)`);
-        
-        //checks if space taken
-        if (Gameboard.board[row][col] !== ""){
-            console.log(`That space is taken, choose again`)
-            continue;
-        }
-    
-        activePlayerMove(row, col);
-        let isGameWon = checkGameWon(activePlayer.marker);
-        if (isGameWon === true){
-           return console.log(`Game over, ${activePlayer.name} wins`); 
-           keepGoing = false;
-        }
+
+        let row = +e.target.dataset.row;
+        let col = +e.target.dataset.col;
+         
+         
+         
+        if (Gameboard.board[row][col] === ""){
+            Gameboard.place_marker(row, col, activePlayer.marker)
+            e.target.textContent = activePlayer.marker;
+           
+           if (checkGameWon(activePlayer.marker)){
+            console.log(`Game over ${activePlayer.name} wins`);
+            gameOver = true;
+           // return;
+           } else if (Gameboard.checkTie()){
+            console.log(`It's a tie`);
+            gameOver = true;
+           // return;
+           }
+           switchPlayer();
+       
+     } else{
+        console.log("That space is taken, choose again")
+     }
+      });
       
-        let isTie = Gameboard.checkTie();
-        if (isTie === true){
-            return console.log("It's a tie");
-            keepGoing = false;
-        }
-
-        switchPlayer();
+      
     
- 
-        
-    }
+    
 }
+startButton.addEventListener("click", playGame);
+
 
    
 
     return {playGame}
 }
   
-let game = gameController();
+gameController();
+
 
